3b795b9607cdcbe39b832a693cdce4c786b5c13c
KartikAkolia <94763117+KartikAkolia@users.noreply.github.com>
2024-07-25 14:13:15 +0000

    Update set-hypr.sh

--- set-hypr.sh
+++ set-hypr.sh
@@ -11,92 +11,75 @@ LOG="install.log"
 set -e
 
 printf "$(tput setaf 2) Welcome to the Arch Linux YAY Hyprland installer!\n $(tput sgr0)"
-
 sleep 2
 
 printf "$YELLOW PLEASE BACKUP YOUR FILES BEFORE PROCEEDING!
-This script will overwrite some of your configs and files!"
-
+This script will overwrite some of your configs and files!\n"
 sleep 2
 
-printf "\n
-$YELLOW  Some commands requires you to enter your password inorder to execute
-If you are worried about entering your password, you can cancel the script now with CTRL Q or CTRL C and review contents of this script. \n"
-
+printf "\n$YELLOW Some commands require you to enter your password.
+If you are worried about entering your password, you can cancel the script now with CTRL Q or CTRL C and review the contents of this script.\n"
 sleep 3
 
-# Check if yay is installed
-ISyay=/sbin/yay
+# Function to print error messages
+print_error() {
+    printf " %s%s\n" "$RED" "$1" >&2
+}
 
-if [ -f "$ISyay" ]; then
-    printf "\n%s - yay was located, moving on.\n" "$GREEN"
-else 
+# Function to print success messages
+print_success() {
+    printf "%s%s\n" "$GREEN" "$1"
+}
+
+# Check if yay is installed
+if ! command -v yay &> /dev/null; then
     printf "\n%s - yay was NOT located\n" "$YELLOW"
-    read -n1 -rep "${CAT} Would you like to install yay (y,n)" INST
+    read -n1 -rep "${CAT} Would you like to install yay (y,n)? " INST
     if [[ $INST =~ ^[Yy]$ ]]; then
         git clone https://aur.archlinux.org/yay.git
         cd yay
         makepkg -si --noconfirm 2>&1 | tee -a $LOG
         cd ..
     else
-        printf "%s - yay is required for this script, now exiting\n" "$RED"
-        exit
+        print_error "yay is required for this script, now exiting"
+        exit 1
     fi
-# update system before proceed
-    printf "${YELLOW} System Update to avoid issue\n" 
-    yay -Syu --noconfirm 2>&1 | tee -a $LOG
 fi
 
-# Function to print error messages
-print_error() {
-    printf " %s%s\n" "$RED" "$1" "$NC" >&2
-}
+# Update system before proceeding
+printf "${YELLOW} System Update to avoid issues\n"
+yay -Syu --noconfirm 2>&1 | tee -a $LOG
 
-# Function to print success messages
-print_success() {
-    printf "%s%s%s\n" "$GREEN" "$1" "$NC"
-}
-
-
-### Install packages ####
-read -n1 -rep "${CAT} Would you like to install the packages? (y/n)" inst
+### Install packages ###
+read -n1 -rep "${CAT} Would you like to install the packages? (y/n)? " inst
 echo
 
-if [[ $inst =~ ^[Nn]$ ]]; then
-    printf "${YELLOW} No packages installed. Goodbye! \n"
-            exit 1
-        fi
-
 if [[ $inst =~ ^[Yy]$ ]]; then
-   git_pkgs="grimblast-git sddm-git hyprpicker-git waybar-hyprland-git"
-   hypr_pkgs="hyprland wl-clipboard wf-recorder rofi wlogout swaylock-effects dunst swaybg kitty"    
-   font_pkgs="ttf-nerd-fonts-symbols-common otf-firamono-nerd inter-font otf-sora ttf-fantasque-nerd noto-fonts noto-fonts-emoji ttf-comfortaa"
-   font_pkgs2="ttf-jetbrains-mono-nerd ttf-icomoon-feather ttf-iosevka-nerd adobe-source-code-pro-fonts"
-   app_pkgs="nwg-look-bin qt5ct btop jq gvfs ffmpegthumbs swww mousepad mpv  playerctl pamixer noise-suppression-for-voice"
-   app_pkgs2="polkit-gnome ffmpeg neovim viewnior pavucontrol thunar ffmpegthumbnailer tumbler thunar-archive-plugin xdg-user-dirs"
-   theme_pkgs="nordic-theme papirus-icon-theme starship "
+    git_pkgs="grimblast-git sddm-git hyprpicker-git waybar-hyprland-git"
+    hypr_pkgs="hyprland wl-clipboard wf-recorder rofi wlogout swaylock-effects dunst swaybg kitty"
+    font_pkgs="ttf-nerd-fonts-symbols-common otf-firamono-nerd inter-font otf-sora ttf-fantasque-nerd noto-fonts noto-fonts-emoji ttf-comfortaa"
+    font_pkgs2="ttf-jetbrains-mono-nerd ttf-icomoon-feather ttf-iosevka-nerd adobe-source-code-pro-fonts"
+    app_pkgs="nwg-look-bin qt5ct btop jq gvfs ffmpegthumbs swww mousepad mpv playerctl pamixer noise-suppression-for-voice"
+    app_pkgs2="polkit-gnome ffmpeg neovim viewnior pavucontrol thunar ffmpegthumbnailer tumbler thunar-archive-plugin xdg-user-dirs"
+    theme_pkgs="nordic-theme papirus-icon-theme starship"
 
     yay -R --noconfirm swaylock waybar
 
     if ! yay -S --noconfirm $git_pkgs $hypr_pkgs $font_pkgs $font_pkgs2 $app_pkgs $app_pkgs2 $theme_pkgs 2>&1 | tee -a $LOG; then
-        print_error " Failed to install additional packages - please check the install.log \n"
+        print_error "Failed to install additional packages - please check the install.log"
         exit 1
     fi
     xdg-user-dirs-update
-    echo
-    print_success " All necessary packages installed successfully."
-
+    print_success "All necessary packages installed successfully."
 else
-    echo
-    print_error " Packages not installed - please check the install.log"
-    sleep 1
+    print_error "No packages installed. Exiting..."
+    exit 1
 fi
 
-
 ### Copy Config Files ###
-read -n1 -rep "${CAT} Would you like to copy config files? (y,n)" CFG
+read -n1 -rep "${CAT} Would you like to copy config files? (y,n)? " CFG
 if [[ $CFG =~ ^[Yy]$ ]]; then
-    printf " Copying config files...\n"
+    printf "Copying config files...\n"
     cp -r dotconfig/dunst ~/.config/ 2>&1 | tee -a $LOG
     cp -r dotconfig/hypr ~/.config/ 2>&1 | tee -a $LOG
     cp -r dotconfig/kitty ~/.config/ 2>&1 | tee -a $LOG
@@ -105,8 +88,8 @@ if [[ $CFG =~ ^[Yy]$ ]]; then
     cp -r dotconfig/swaylock ~/.config/ 2>&1 | tee -a $LOG
     cp -r dotconfig/waybar ~/.config/ 2>&1 | tee -a $LOG
     cp -r dotconfig/wlogout ~/.config/ 2>&1 | tee -a $LOG
-    
-    # Set some files as exacutable 
+
+    # Set some files as executable 
     chmod +x ~/.config/hypr/xdg-portal-hyprland
     chmod +x ~/.config/waybar/scripts/waybar-wttr.py
 fi
@@ -118,48 +101,46 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.1/CascadiaCo
 unzip '*.zip' -d $HOME/Downloads/nerdfonts/
 rm -rf *.zip
 sudo cp -R $HOME/Downloads/nerdfonts/ /usr/share/fonts/
-
 fc-cache -rv  
 
 ### Enable SDDM Autologin ###
-read -n1 -rep 'Would you like to enable SDDM autologin? (y,n)' SDDM
-if [[ $SDDM == "Y" || $SDDM == "y" ]]; then
+read -n1 -rep 'Would you like to enable SDDM autologin? (y,n)? ' SDDM
+if [[ $SDDM =~ ^[Yy]$ ]]; then
     LOC="/etc/sddm.conf"
     echo -e "The following has been added to $LOC.\n"
-    echo -e "[Autologin]\nUser = $(whoami)\nSession=hyprland" | sudo tee -a $LOC
-    echo -e "\n"
-    echo -e "Enabling SDDM service...\n"
+    echo -e "[Autologin]\nUser=$(whoami)\nSession=hyprland" | sudo tee -a $LOC
+    echo -e "\nEnabling SDDM service...\n"
     sudo systemctl enable sddm
     sleep 3
 fi
 
-# BLUETOOTH
-read -n1 -rep "${CAT} OPTIONAL - Would you like to install Bluetooth packages? (y/n)" BLUETOOTH
+### Install Bluetooth Packages ###
+read -n1 -rep "${CAT} OPTIONAL - Would you like to install Bluetooth packages? (y,n)? " BLUETOOTH
 if [[ $BLUETOOTH =~ ^[Yy]$ ]]; then
-    printf " Installing Bluetooth Packages...\n"
- blue_pkgs="bluez bluez-utils blueman"
+    printf "Installing Bluetooth Packages...\n"
+    blue_pkgs="bluez bluez-utils blueman"
     if ! yay -S --noconfirm $blue_pkgs 2>&1 | tee -a $LOG; then
-       	print_error "Failed to install bluetooth packages - please check the install.log"    
-    printf " Activating Bluetooth Services...\n"
-    sudo systemctl enable --now bluetooth.service
-    sleep 2
+        print_error "Failed to install Bluetooth packages - please check the install.log"
+    else
+        printf "Activating Bluetooth Services...\n"
+        sudo systemctl enable --now bluetooth.service
+        sleep 2
     fi
 else
-    printf "${YELLOW} No bluetooth packages installed..\n"
-	fi
+    printf "${YELLOW} No Bluetooth packages installed.\n"
+fi
 
-    
 ### Script is done ###
 printf "\n${GREEN} Installation Completed.\n"
 echo -e "${GREEN} You can start Hyprland by typing Hyprland (note the capital H).\n"
-read -n1 -rep "${CAT} Would you like to start Hyprland now? (y,n)" HYP
+read -n1 -rep "${CAT} Would you like to start Hyprland now? (y,n)? " HYP
 if [[ $HYP =~ ^[Yy]$ ]]; then
-    if command -v Hyprland >/dev/null; then
+    if command -v Hyprland &> /dev/null; then
         exec Hyprland
     else
-         print_error " Hyprland not found. Please make sure Hyprland is installed by checking install.log.\n"
+        print_error "Hyprland not found. Please make sure Hyprland is installed by checking install.log."
         exit 1
     fi
 else
-    exit
+    exit 0
 fi
